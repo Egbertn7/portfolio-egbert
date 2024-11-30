8d8500547bef12f98e19f0ea1e00faccde3a8b01
Egbert Angenius <piterwang16@gmail.com>
2024-11-30 10:18:48 +0700

    initial-commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolioku",  // Pastikan ini sesuai dengan URL GitHub Pages Anda
+  base: "/portfolio-egbert/",
   plugins: [react()],
-  build: {
-    outDir: 'build',  // Mengubah output build ke folder 'build'
-  },
 });
+
+
